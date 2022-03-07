b1ba1cc2b571fef5bf0fc9741aeb58fcf63e1701
s163928 <96971513+s163928@users.noreply.github.com>
2022-03-07 13:34:28 +0100

    try to fix pipeline

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'test',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '98bb9ed9c5ceac37d32885cb3a447d2dd7f7b7d0',
+        'reference' => '68a6811db279d1264c2ec9ec3c63c1520286b579',
         'name' => 'butler_hjemmeside/composer',
         'dev' => true,
     ),
@@ -16,7 +16,7 @@
             'type' => 'test',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '98bb9ed9c5ceac37d32885cb3a447d2dd7f7b7d0',
+            'reference' => '68a6811db279d1264c2ec9ec3c63c1520286b579',
             'dev_requirement' => false,
         ),
         'doctrine/instantiator' => array(
