ded423f5552f783a71020772e65d3e46b6eb50fc
Maja Larsen <maalmaja@gmail.com>
2022-03-07 18:46:33 +0100

    changing all files to php files

--- app/index.php
+++ app/index.php
@@ -1,13 +1,26 @@
- <?php 
+<?php 
     $conn = new mysqli("localhost:3306", "admin", "admin", "butlerdb");
 ?>
-<html>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-    <title>PHP Test</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/stylesheet.css">
+    <title>web app</title>
 </head>
 
 <body>
     <?php echo '<p>Hello woo</p>'; ?>
+    <div>
+        <h1>Login</h1>
+        <button><a href="Tidsregistrering/tidsregistrering.html">Login</a></button>
+    </div>
+
+    <!-- <script src="index.js"></script> -->
 </body>
 
 </html>
