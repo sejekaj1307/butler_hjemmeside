f74047c6e50711a9578c06ae3e7942c845012841
Maja Larsen <maalmaja@gmail.com>
2022-05-18 22:49:53 +0200

    finishing style for harmonica list on planned_servicw

--- app/Medarbejdere/medarbejdere.js
+++ app/Medarbejdere/medarbejdere.js
@@ -1,10 +1,16 @@
+//Media query
+let screen_width = window.matchMedia("(max-width: 1200px)")
+
 
 function open_close_employee_info(id, drop_down_classname) {
-    let employee_dropdown_mobile = document.querySelectorAll("." + drop_down_classname)
-    if (employee_dropdown_mobile[id-1].style.display === "none" || employee_dropdown_mobile[id-1].style.display === "") {
-        employee_dropdown_mobile[id-1].style.display = "flex";
-    } else {
-        employee_dropdown_mobile[id-1].style.display = "none";
+    if(screen_width.matches) {
+        console.log(screen_width)
+        let employee_dropdown_mobile = document.querySelectorAll("." + drop_down_classname)
+        if (employee_dropdown_mobile[id-1].style.display === "none" || employee_dropdown_mobile[id-1].style.display === "") {
+            employee_dropdown_mobile[id-1].style.display = "flex";
+        } else {
+            employee_dropdown_mobile[id-1].style.display = "none";
+        }
     }
 }
 
