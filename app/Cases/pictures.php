f126db19b2f7157d1ab1d612b3b39c359bec45b9
Maja Larsen <maalmaja@gmail.com>
2022-05-25 18:05:03 +0200

    adding pictures and files to describe_case

--- app/Cases/pictures.php
+++ app/Cases/pictures.php
@@ -20,7 +20,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles/web/styles.css">
-    <title>Beskriv sag</title>
+    <title>Billeder</title>
 </head>
 
 <body>
@@ -144,43 +144,13 @@
 
         <div class="case_list_page">
             <div class="describe_case_navbar">
-                <button class="describe_case_navbar_active"><a href="describe_case.php">Sagsinfo</a></button>
-                <button><a href="pictures.php">Billeder</a></button>
+                <button><a href="describe_case.php">Sagsinfo</a></button>
+                <button class="describe_case_navbar_active"><a href="pictures.php">Billeder</a></button>
                 <button><a href="files.php">Filer</a></button>
             </div>
             <div class="describe_case_info_container">
-                <h1>Sagsinfo - sag nr #</h1>
+                <h1>Billeder - sag nr #</h1>
                 <div class="input_container">
-                    <div class="top_inputs_container">
-                        <div class="small_inputs"><p>Kunde :</p><input type="text"></div>
-                        <div class="small_inputs"><p>Kundesag nr :</p><input type="text"></div>
-                        <div class="small_inputs"><p>Intern sag nr. :</p><input type="text"></div>
-                        <div class="small_inputs"><p>Ansvarlig :</p><input type="text"></div>
-                        <div class="large_inputs"><p>Tilkørsel - pladsforhold, adgang, tid, støj, mm.</p><textarea type="subject"></textarea></div>
-                    </div>
-                    <div class="top_inputs_container">
-                        <div class="small_inputs"><p>Lokation :</p><input type="text"></div>
-                        <div class="small_inputs"><p>Postnummer :</p><input type="text"></div>
-                        <div class="small_inputs"><p>Maskiner :</p><input type="text"></div>
-                        <div class="small_inputs"><p>Medarbejder :</p><input type="text"></div>
-                        <div class="large_inputs"><p>Ekstra arbejde/ventetid</p><textarea type="subject"></textarea></div>
-                    </div>
-                </div>
-                <div class="bottom_inputs_container">
-                    <div class="date_and_status">
-                        <p>Forventet start d.: 02-02-22</p>
-                        <p>Forventet slut d. : 02-02-22</p>
-                        <div class="status_container"><p>Status :</p><input type="text" name="" id=""></div>    
-                    </div>
-                    <div class="job_type_container">
-                        <label for="jobtypes">Job typer :</label>
-                        <select name="jobtypes" id="jobtypes" class="job_type_select">
-                            <option value="CPT">CPT</option>
-                            <option value="Miljø">Miljø</option>
-                            <option value="GPS_afsætning">GPS afsætning</option>
-                        </select>
-                    </div>
-                </div>
             </div>
         <div>
 
@@ -196,35 +166,6 @@
         ----------------------------->
         <div class="pop_up_modal" style="display: <?php echo $display_describe_case_pop_up ?>">
             Opdater sagen?
-            <!-- <h3>Beskriv sag</h3>
-            <div class="pop-up-row"><p>Sagssnr. : </p><input type="text" name="case_nr_u" value="<?php echo isset($case_nr) ? $case_nr : '' ?>"></div>
-            <div class="pop-up-row">
-                <p>Ansvarlig : </p>
-                <select name="case_responsible_u">
-                    <?php
-                        foreach($case_responsible_initials_list as $case_responsible_initials){
-                            echo "<option " . ($case_responsible == $case_responsible_initials ? 'selected' : '') . " value=" . $case_responsible_initials . ">" . $case_responsible_initials . "</option>";
-                        }
-                    ?>
-                </select>
-            </div>
-            <div class="pop-up-row">
-                    <p>Status : </p>
-                    <select name="status_u">
-                        <option <?php echo $status == "Oprettet" ? 'selected' : '' ?> value="Oprettet">Oprettet</option>
-                        <option <?php echo $status == "Beskrevet" ? 'selected' : '' ?> value="Beskrevet">Beskrevet</option>
-                        <option <?php echo $status == "Aktiv" ? 'selected' : '' ?> value="Aktiv">Aktiv</option>
-                        <option <?php echo $status == "Fuldført" ? 'selected' : '' ?> value="Fuldført">Fuldført</option>
-                    </select>
-                </div>   
-            <div class="pop-up-row"><p>Lokation : </p><input type="text" name="location_u" value="<?php echo isset($location) ? $location : '' ?>"></div>
-            <div class="pop-up-row"><p>Startdato : </p><input type="text" name="est_start_date_u" value="<?php echo isset($est_start_date) ? $est_start_date : '' ?>"></div>
-            <div class="pop-up-row"><p>Deadline : </p><input type="text" name="est_end_date_u" value="<?php echo isset($est_end_date) ? $est_end_date : '' ?>"></div>
-            <div class="pop-up-btn-container">
-                <input type="submit" name="knap" value="Annuller" class="pop_up_cancel">
-                <input type="submit" name="knap" value="Opdater" class="pop_up_confirm">
-            </div>
-        </div> -->
         </div>
         
     </form>
